8e1f0f2c0e292446b0c90c25305f85e91cd0fa6e
Abhishek Jha <abshkj247@gmail.com>
2025-09-26 10:42:27 -0400

    Update main.py

--- main/main.py
+++ main/main.py
@@ -48,17 +48,7 @@ def main(gpu,  ngpus_per_node, config, args):
             rank=args.local_rank,
             world_size=args.world_size
         )
-        if args.gcloud:
-            if args.local_rank <= 0:
-                logger.info("downloading voc dataset ...")
-                download_voc_unzip(data_dir="./VOCtrainval_11-May-2012", prefix="your/prefix/")
-                logger.info("downloading resnet {} pretrained checkpoint ...".format(str(args.backbone)))
-                download_checkpoint("VocCode/Model/PSPNet/Backbones/pretrained/",
-                                    '3x3resnet{}-imagenet.pth'.format(str(args.backbone)))
-                
-                download_checkpoint("VocCode/Model/Deeplabv3_plus/Backbones/pretrained/",
-                                    'resnet{}.pth'.format(str(args.backbone)))
-            dist.barrier()
+
 
     random.seed(42)
     torch.manual_seed(42)
@@ -138,9 +128,6 @@ if __name__ == '__main__':
 
     parser.add_argument('-g', '--gpus', default=2, type=int,
                         help='number of gpus per node')
-
-    parser.add_argument('--gcloud', action='store_true',
-                        help='using gcloud or not.')
 
     parser.add_argument("--local_rank", type=int, default=-1)
 
